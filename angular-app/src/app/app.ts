9cf0f3cf61485f01ffceb0055cf5537e62cc8242
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-11-13 22:14:29 -0800

    updates

--- angular-app/src/app/app.ts
+++ angular-app/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('shiftbuilder-angular');
+  protected readonly title = signal('Shift Builder');
 }
